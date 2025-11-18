5dc8d86c2b261540d4e0164d3c20f5096c0814d5
fureiyaa <farizdzu16@gmail.com>
2025-11-18 10:44:12 +0700

    push

---
+++ lib/halaman_dua.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'halaman_tiga.dart';
+
+class HalamanDua extends StatelessWidget {
+  const HalamanDua({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text
+      ('Halaman 2')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment:
+           MainAxisAlignment.center,
+           children: [
+            ElevatedButton(
+              onPressed:() {
+              Navigator.pop(context);
+            },
+            child: Text('Kembali Kehalaman Sebelumnya'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: 
+              (context) => HalamanTiga()),
+              );
+            },
+            )
+
+           
+           ],
+        ),
+      ),
+    );
+  }
+}
