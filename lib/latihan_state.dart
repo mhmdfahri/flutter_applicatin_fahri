f963086b67b7ef030e2741f5e73e78c60e140d49
fureiyaa <farizdzu16@gmail.com>
2025-11-18 11:44:54 +0700

    pust

---
+++ lib/latihan_state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LatihanState extends StatefulWidget {
+  const LatihanState({super.key});
+
+  @override
+  State<LatihanState> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<LatihanState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
